f5227893eeb33e2579f6d74fb5fdbd2996718ddb
mushon <mushon@shual.com>
2011-02-19 16:25:08 +0200

    fix for image page redirection

--- image.php
+++ image.php
@@ -1,1 +1,5 @@
-<?php wp_redirect(get_permalink($post->post_parent)); ?>
+<?php
+//Skip the image attachment page, go directly to the file.
+$attachment = wp_get_attachment_image_src($post->ID, 'full');
+wp_redirect($attachment[0]);
+?>
